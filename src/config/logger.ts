789058711cac2e5d12ea2ee0037e3e3cff7e565f
madai2021 <madai21.info@gmail.com>
2026-02-15 01:26:54 +0900

    Loggerの引数をany型から修正

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -2,21 +2,21 @@ import { ENV } from "./env";
 
 class Logger {
   // infoログ（開発モードのみ表示）
-  static info(...args: any[]) {
+  static info(...args: Parameters<typeof console.info>) {
     if (ENV.isDevelopment) {
       console.info("[INFO]", ...args);
     }
   }
 
   // warnログ（開発モードのみ表示）
-  static warn(...args: any[]) {
+  static warn(...args: Parameters<typeof console.warn>) {
     if (ENV.isDevelopment) {
       console.warn("[WARN]", ...args);
     }
   }
 
   // errorログ（本番でも表示）
-  static error(...args: any[]) {
+  static error(...args: Parameters<typeof console.error>) {
     console.error("[ERROR]", ...args);
   }
 }
