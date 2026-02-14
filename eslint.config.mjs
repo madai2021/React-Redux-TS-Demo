9daca17cc0d8c4699fc081180da23dcd0d003906
madai2021 <madai21.info@gmail.com>
2026-02-15 01:53:56 +0900

    ESLintの設定を修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import js from "@eslint/js";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import githubPlugin from "eslint-plugin-github";
+import jsdocPlugin from "eslint-plugin-jsdoc";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
@@ -14,6 +16,8 @@ export default [
     },
     plugins: {
       "@typescript-eslint": tsPlugin,
+      jsdoc: jsdocPlugin,
+      github: githubPlugin,
       react,
       "react-hooks": reactHooks,
     },
@@ -24,6 +28,8 @@ export default [
       "@typescript-eslint/no-explicit-any": "error",
       "no-undef": "off",
       "no-unused-vars": "off",
+      "jsdoc/require-returns-check": "warn",
+      "github/no-then": "warn",
     },
     settings: {
       react: { version: "detect" },
